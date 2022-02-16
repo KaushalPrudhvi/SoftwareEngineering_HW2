910ede4ad0cc555e14554bbf724ecb354f9a1f04
Kaushal <kausht14@gmail.com>
2022-02-16 16:22:22 +0000

    part 2 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,19 +8,43 @@ class MoviesController < ApplicationController
 
   def index
     @movies = Movie.all
+    #get Movie ratings
+    @all_ratings = Movie.movieRatings
+    
+    @ratingsChecked = params[:ratings]
+    @prevSession = session[:filter]
+    if @ratingsChecked
+      #get keys of the checked ratings
+      @ratings= params[:ratings].keys
+      #remeber in sessions what keys have been previously checked
+      session[:filter] = @ratings
+    elsif session[:filter]
+      #restore previous session ratings 
+      @ratings = @prevSession
+    else
+      @ratings = @all_ratings
+    end
     #added on 15-02
+    #where is used to filter queries so that we can get only those which we have specified 
+    #where! is destructive in nature
+    @movies.where!(:rating => @ratings)
     
     @sortby= params[:target]
     if @sortby=='title'
       @title_header= 'hilite'
       sortedby = {:sorted => :title }
+      @movies = Movie.order @sortby
     elsif @sortby=='release_date'
       @release_date_header= 'hilite'
       sortedby ={ :sorted => :release_date }
+      @movies = Movie.order @sortby
     else
     end
     #@movies = Movie.order sortby
-    @movies = Movie.order params[:target]
+    
+    # 1. how to figure out which boxes the user checked and 
+    # 2. how to restrict the database query based on that result.
+    
   end
 
   def new
