a4dc541016fdb26120727612144e0ca34e61d899
Kaushal <kausht14@gmail.com>
2022-02-16 20:50:18 +0000

    rollback

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,30 +9,65 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
     #get Movie ratings
+
     @all_ratings = Movie.movieRatings
+
+    if params[:targetSort].nil? && params[:ratings].nil? && (session[:targetSort].nil? || !session[:ratings].nil?)
+       redirect_to movies_path({:targetSort => session[:targetSort], :ratings => session[:ratings]})
+    end
+    
+    
+      
     
     @ratingsChecked = params[:ratings]
-    @prevSession = session[:filter]
+    @prevCheckedSession = session[:ratings]
     if @ratingsChecked
       #get keys of the checked ratings
       @ratings= params[:ratings].keys
+      
       #remeber in sessions what keys have been previously checked
-      session[:filter] = @ratings
-    elsif session[:filter]
+      session[:ratings] = @ratingsChecked
+    elsif session[:ratings]
       #restore previous session ratings 
-      @ratings = @prevSession
+      @ratings = session[:ratings].keys
+      
     else
       @ratings = @all_ratings
+      
     end
+    
     #added on 15-02
     #where is used to filter queries so that we can get only those which we have specified 
     #where! is destructive in nature
     @movies.where!(:rating => @ratings)
+
     
-    @sortby= params[:target]
+    @sortby= params[:targetSort] 
+    
+    if @sortby
+    
+    
+      if @sortby== 'title'
+        @title_header= 'hilite'
+        @movies = Movie.order("#{@sortby} ASC")
+        session[:targetSort]=  "title"
+        
+      elsif @sortby== 'release_date'
+        @release_date_header= 'hilite'
+        @movies = Movie.order("#{@sortby} ASC")
+        session[:targetSort]=  "release_date"
+      
+      end
+    end
+      
+    
+    
+  end
+=begin  
     if @sortby=='title'
       @title_header= 'hilite'
       sortedby = {:sorted => :title }
+      
       @movies = Movie.order @sortby
     elsif @sortby=='release_date'
       @release_date_header= 'hilite'
@@ -40,13 +75,14 @@ class MoviesController < ApplicationController
       @movies = Movie.order @sortby
     else
     end
+
     #@movies = Movie.order sortby
     
     # 1. how to figure out which boxes the user checked and 
     # 2. how to restrict the database query based on that result.
     
   end
-
+=end
   def new
     # default: render 'new' template
   end
